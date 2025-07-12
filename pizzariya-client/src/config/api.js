b77f1dbd3a8a29854b36aa1ef1c9bb1041f56add
deep <dks.22u10497@btech.nitdgp.ac.in>
2025-07-12 19:30:41 +0530

    try to host

--- pizzariya-client/src/config/api.js
+++ pizzariya-client/src/config/api.js
@@ -1,6 +1,5 @@
 // API Configuration
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL;
-//    || "http://localhost:4000";
+  process.env.REACT_APP_API_BASE_URL || "https://your-backend-url.onrender.com";
 
 export default API_BASE_URL;
