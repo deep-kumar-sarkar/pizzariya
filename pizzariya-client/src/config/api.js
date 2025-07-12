33c7dc7b8c18949574f6764aac525e3cd38fac70
deep <dks.22u10497@btech.nitdgp.ac.in>
2025-07-12 19:16:40 +0530

    configure base url

--- pizzariya-client/src/config/api.js
+++ pizzariya-client/src/config/api.js
@@ -1,5 +1,6 @@
 // API Configuration
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:4000";
+  process.env.REACT_APP_API_BASE_URL;
+//    || "http://localhost:4000";
 
 export default API_BASE_URL;
